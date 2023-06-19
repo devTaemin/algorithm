9fdef91a46ca48bac970de474d5bf899086c11ce
Taemin Im <devTaemin@gmail.com>
2023-06-20 03:30:23 +0900

    Create 027433_팩토리얼2.java

---
+++ baekjoon/027433_팩토리얼2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+
+        System.out.println(factorial(n));
+    }
+    public static long factorial(long n) {
+        if (n <= 1) return 1;
+
+        return n * factorial(n-1);
+    }
+}
